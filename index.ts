080c7ec3fbeb40e3b0653949934c83f0ecc81127
Tadej Vengust <vengust.tadej@gmail.com>
2019-12-12 15:19:56 +0100

    Update style.

--- index.ts
+++ index.ts
@@ -1,7 +1,38 @@
 import { enableMetamask } from "./src/example";
 
 const btnEnableMetamask = document.getElementById("btnEnableMetamask");
+const console = document.getElementById("console");
 
 btnEnableMetamask.addEventListener("click", async () => {
-  await enableMetamask();
+  printMessage("Enabling metamask.");
+  let error = null;
+  try {
+    await enableMetamask();
+  } catch (e) {
+    error = e;
+  }
+  if (error) {
+    printError(error);
+  } else {
+    printMessage("Metamask enabled.");
+  }
 });
+
+function printMessage(message: any) {
+  if (typeof message !== "string") {
+    message = JSON.stringify(message, null, 2);
+  }
+  const div = document.createElement("div");
+  div.innerText = message;
+  console.prepend(div);
+}
+
+function printError(message: any) {
+  if (typeof message !== "string") {
+    message = JSON.stringify(message, null, 2);
+  }
+  const div = document.createElement("div");
+  div.innerText = "Error: " + message;
+  div.className = "error";
+  console.prepend(div);
+}
